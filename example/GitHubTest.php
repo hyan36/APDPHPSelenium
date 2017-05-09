4a1cd576621d5208517ec8dcd7a4c1f54221220a
Haihao Yan <hyan@apdgroup.com>
2017-05-09 19:12:25 +1000

    initial commit

---
+++ example/GitHubTest.php
@@ -0,0 +1,30 @@
+<?php
+class GitHubTest extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @var \RemoteWebDriver
+	 */
+	protected $webDriver;
+	
+	public function setUp()
+	{
+		$capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => 'firefox');
+		$this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
+	}
+	
+	protected $url = 'https://github.com';
+	
+	public function testGitHubHome()
+	{
+		$this->webDriver->get($this->url);
+		
+		$form = $this->webDriver->findElement(WebDriverBy::cssSelector(".js-site-search-form"));
+		$field = $form->findElement(WebDriverBy::cssSelector(".header-search-input"));
+		$field->sendKeys("hyan36");
+		$form->submit();
+		// checking that page title contains word 'GitHub'
+		$this->assertContains('GitHub', $this->webDriver->getTitle());
+		//$this->webDriver->close();
+	}
+	
+}
